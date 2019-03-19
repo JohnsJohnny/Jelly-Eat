fa0fadaae0a2c12a6d4e1c9ef45aa9aefeb82cf9
Johnny Gomes <37989560+JohnsJohnny@users.noreply.github.com>
2019-03-19 15:36:02 -0300

    Add files via upload

---
+++ SoundsOfColision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsOfColision : MonoBehaviour
+{
+
+
+    public static SoundsOfColision Instance;
+    public AudioClip AudioMaca;
+
+    public void MakeAudioMaca()
+    {
+        MakeSound (AudioMaca);
+    }
+    private void MakeSound(AudioClip originalClip)
+    {
+        AudioSource.PlayClipAtPoint(originalClip, transform.position);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        if (collider.gameObject.tag == "Player")
+        {
+
+        }
+
+    }
+
+}
